b8fc9a3817a61d9f7ce555e68afc835cc488d8a2
Ivan Vecera <ivan@cera.cz>
2024-07-03 11:40:30 +0200

    gralloc: Allow devices to opt-in for YCrCb camera video encode
    DeviceAsWebcam is using USAGE_VIDEO_ENCODE [1] for buffers and
    this results in combined usage: (VIDEO_ENCODER | CAMERA_OUTPUT).
    
    On some Qcom devices (verified on pyxis (sdm710)) and maybe others
    DeviceAsWebcam has red & blue colors swapped with HAL_PIXEL_FORMAT_YCbCr_420_SP_VENUS.
    
    When using HAL_PIXEL_FORMAT_YCrCb_420_SP_VENUS the output from
    DeviceAsWebcam has correct colors.
    
    [1] https://android.googlesource.com/platform/packages/services/DeviceAsWebcam/+/d079a82ba4f773210c01d488d2dfa1d4d25be8e2
    
    Change-Id: I4376102e2b0eeaefed536eff29c315a71e469b57

--- gralloc/Android.mk
+++ gralloc/Android.mk
@@ -52,6 +52,9 @@ endif
 ifeq ($(TARGET_USES_UNALIGNED_YCRCB),true)
     LOCAL_CFLAGS              += -DUSE_UNALIGNED_YCRCB
 endif
+ifeq ($(TARGET_USES_YCRCB_CAMERA_ENCODE),true)
+    LOCAL_CFLAGS              += -DUSE_YCRCB_CAMERA_ENCODE
+endif
 
 LOCAL_ADDITIONAL_DEPENDENCIES := $(common_deps)
 LOCAL_SRC_FILES               := gr_utils.cpp gr_adreno_info.cpp
